360bbc93de6cdeea761f870a064bc39b255124ec
Daniel Cols <dcols@uncc.edu>
2023-02-01 22:26:00 -0500

    pushing files

--- WeatherPy/Starter_Code/starter_code/api_keys.py
+++ WeatherPy/Starter_Code/starter_code/api_keys.py
@@ -1,5 +1,5 @@
 # OpenWeatherMap API Key
-weather_api_key = "YOUR KEY HERE"
+weather_api_key = "84ecd30f598c60898b43fd218c3bcab0"
 
 # Geoapify API Key
-geoapify_key = "YOUR KEY HERE"
+geoapify_key = "f9498bcdcf644611b70ff16dfcb6ae67"
